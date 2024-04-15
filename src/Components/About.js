42e79c50aebe69b2e3e922c61c9893d5f6223994
Rohit <rsabi2708@gmail.com>
2024-04-15 11:21:39 +0530

    Context API with useEffect - 2

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,11 +1,14 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import noteContext from "../context/notes/noteContext";
 function About() {
   const a = useContext(noteContext);
+  useEffect(() => {
+    a.update();
+  }, []);
   return (
     <div>
       <h2>About</h2>
-      This is About {a.name} & he is class {a.class}
+      This is About {a.state.name} & he is class {a.state.class}
     </div>
   );
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-  const state = {
+  const s1 = {
     name: "Rohit",
     class: "5f",
   };
+  let [state, setState] = useState(s1);
+  const update = () => {
+    setTimeout(() => {
+      state = {
+        name: "Rahul",
+        class: "10f",
+      };
+      setState(state);
+    }, 1500);
+  };
+
   return (
-    <NoteContext.Provider value={state}>{props.children}</NoteContext.Provider>
+    <NoteContext.Provider value={{ state, update }}>
+      {props.children}
+    </NoteContext.Provider>
   );
 };
 
